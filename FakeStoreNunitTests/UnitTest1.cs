cad254a421860ce672ce22b02a9311ef673561ad
Wellington <wellingtonicoletti@gmail.com>
2024-12-13 14:21:54 -0300

    primeiro commit

---
+++ FakeStoreNunitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace FakeStoreNunitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
