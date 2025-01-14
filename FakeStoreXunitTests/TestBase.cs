f6cd60d02d2971398fb3b6525fbcb987980c8f03
WellingtonNico <wellingtonicoletti@gmail.com>
2025-01-14 18:06:04 -0300

    tests: simplificada classe base de teste

--- FakeStoreXunitTests/TestBase.cs
+++ FakeStoreXunitTests/TestBase.cs
@@ -8,50 +8,37 @@ using Microsoft.Extensions.DependencyInjection;
 namespace FakeStoreXunitTests;
 
 /// <summary>
-/// Responsável por inicializar a factory e criar o banco de dados
-/// </summary>
-public class TestBaseSetUp
-{
-    public CustomApplicationFactory<Program> Factory = new();
-
-    public TestBaseSetUp()
-    {
-        using var scope = Factory.Services.CreateScope();
-        using var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-        dbContext.Database.EnsureDeleted();
-        // se estivermos usando migrations, chamar dbContext.Database.Migrate pois o coverage pega as migrations
-        dbContext.Database.EnsureCreated();
-    }
-}
-
-/// <summary>
-/// Serve como storage para que as classes de setup iniciem somente uma vez
-/// </summary>
-public class SharedSetUps
-{
-    // TODO: explicar porque não usar com IClassFixture na classe base de teste
-    public static readonly TestBaseSetUp TestBaseSetUp = new();
-    public static readonly Fakers Fakers = new();
-}
-
-/// <summary>
 /// Contém tudo o que é necessário para os testes de integração
 /// também é responsável pela limpeza do banco depois de cada teste
 /// </summary>
 public abstract class TestBase : IAsyncLifetime
 {
     private IDbContextTransaction _transaction;
-    public CustomApplicationFactory<Program> Factory = SharedSetUps.TestBaseSetUp.Factory;
-    public Fakers Fakers = SharedSetUps.Fakers;
-    public IServiceScope Scope;
-    public AppDbContext DbContext;
-    public HttpClient Client;
+    public static readonly CustomApplicationFactory<Program> Factory = new();
+    public static readonly Fakers Fakers = new();
+    public readonly IServiceScope Scope;
+    public readonly AppDbContext DbContext;
+    public readonly HttpClient Client;
+
+    static TestBase()
+    {
+        CreateDatabase();
+    }
 
     public TestBase()
     {
         Scope = Factory.Services.CreateAsyncScope();
         Client = Factory.CreateClient();
         DbContext = Scope.GetService<AppDbContext>();
+    }
+
+    public static void CreateDatabase()
+    {
+        using var scope = Factory.Services.CreateAsyncScope();
+        using var dbContext = scope.GetService<AppDbContext>();
+        dbContext.Database.EnsureDeleted();
+        // se estivermos usando migrations, chamar dbContext.Database.Migrate pois o coverage pega as migrations
+        dbContext.Database.EnsureCreated();
     }
 
     public async Task InitializeAsync()
